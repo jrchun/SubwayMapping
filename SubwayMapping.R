fe893125e277f7d9719a8fe7157c9be4b9dbaca2
jrchun <jrchun1@naver.com>
2018-11-13 15:33:26 +0900

    Merge data 1

--- SubwayMapping.R
+++ SubwayMapping.R
@@ -1,9 +1,22 @@
 rm(list = ls())
 setwd('C:\\github\\Project\\SubwayMapping')
 data <- read.csv('subway.csv')
+data$전철역명
+theater <- read.csv('theater.csv', sep = '\t')
+theater[is.na(theater)] <- 0
+seoulsubway <- as.vector(unique(theater$'지하철역'))
 
-colnames(data)
 sum(is.na(data[,'X좌표']))
-
 data <- data[!is.na(data[,'X좌표']), ]
-unique(data[,'전철역명'])
+data$전철역명 <- as.vector(data$전철역명)
+
+seoulsubway[which(seoulsubway == '총신대입구역/이수역')] <- '총신대입구(이수)역'
+seoulsubway[which(seoulsubway == '양평역')] <- '양평(경의중앙선)역'
+data$전철역명[which(data$전철역명 == '서울(경의중앙선)')] <- '서울'
+data$전철역명[which(data$전철역명 == '신촌(경의중앙선)')] <- '신촌'
+
+data$전철역명[which(data$전철역명 == '효창공원앞')] <- '신촌'
+  
+data$전철역명 <- paste(data$전철역명, '역', sep = "")
+data <- subset(data, data$'전철역명' %in% seoulsubway)
+unique(data$'전철역명')
